a6667501eaf4911c1b8ad88b70dcd77fdfd91d3b
evan999 <emeshberge@gmail.com>
2019-03-08 23:48:21 -0500

    Rename to more descriptive variable names

--- BoardGame/Board.cs
+++ BoardGame/Board.cs
@@ -13,13 +13,38 @@ namespace BoardGame
         // array for cells
         // private char Symbol[] symbols;
 
-        // Draws a single cell to the game board
+        // Draws a game board of x * y dimensions
         public void DrawCell()
         {
+            string inputRows;
+            string inputColumns;
+            int rows, columns;
+            Console.Write("Enter a value for x");
+            inputRows = Console.ReadLine();
+            rows = Convert.ToInt32(inputRows);
+            Console.Write("Enter a value for y");
+            inputColumns = Console.ReadLine();
+            columns = Convert.ToInt32(inputColumns);
+
             // Print characters
-            Console.WriteLine(" " + "-" + " ");
-            Console.WriteLine("|" + " " + "|");
-            Console.WriteLine(" " + "-" + " ");
+            for (int row = 0; row < 4; row++)
+            {
+                for (int column = 0; column < 4; col++)
+                {
+                    if (row % 2 == 0 || row == 0)
+                    {
+                        Console.WriteLine(" " + "-" + " ");
+                    }
+                    else if (row % 2 != 0)
+                    {
+                        Console.WriteLine("|" + " " + "|");
+                    }
+                }
+                
+                // Console.WriteLine(" " + "-" + " ");
+                // Console.WriteLine("|" + " " + "|");
+                // Console.WriteLine(" " + "-" + " ");
+            }
             /*
             for (col = 0; col < 2; col++)
             {
