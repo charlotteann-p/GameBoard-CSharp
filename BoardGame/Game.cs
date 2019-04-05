b34d6dd68a4f0b9ddf7900ce2a40a7cfa0d59d3b
evan999 <emeshberge@gmail.com>
2019-04-04 20:22:09 -0400

    Create Game class and define object properties

---
+++ BoardGame/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGame
+{
+    public class Game : Board
+    {
+        // Properties of the game
+        public string player1;
+        public string player2;
+
+        // var cellState = new List<string>();
+
+        public string[] cellState = { "Red", "Black", null };
+        // string cells = new List<string>();
+        public string[] cells;
+
+        bool gameOver;
+
+        public DateTime GameTime { get; set; }
+
+    }
+}
